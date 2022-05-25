83fc4dae0aef93bf3c26db42300a293d17af9ece
hbashdevv <105935953+hbashdevv@users.noreply.github.com>
2022-05-25 16:04:00 +0300

    Update ميمزز.py

--- ميمزز.py
+++ ميمزز.py
@@ -16,7 +16,7 @@ PING_PIC = os.environ.get("PING_PIC") or (
 )
 
 JM_TXT = os.environ.get("PING_TEXT") or "تم اختيار بصمة 😂"
-$themes = rand(74, 154);
+
 URL = os.environ.get("URL") or "https://t.me/fasngon/$themes"
 
 
