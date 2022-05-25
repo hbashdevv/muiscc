07fe64c4d4041e6402acc7d4f146bd703b22cf1f
hbashdevv <105935953+hbashdevv@users.noreply.github.com>
2022-05-25 16:13:58 +0300

    Update ميمزز.py

--- ميمزز.py
+++ ميمزز.py
@@ -17,7 +17,7 @@ PING_PIC = os.environ.get("PING_PIC") or (
 
 JM_TXT = os.environ.get("PING_TEXT") or "تم اختيار بصمة 😂"
 
-URL = os.environ.get("URL") or "https://t.me/fasngon/$themes"
+URL = os.environ.get("URL") or "https://t.me/fasngon/13114"
 
 
 @jmthon.ar_cmd(pattern="ميمز$")
